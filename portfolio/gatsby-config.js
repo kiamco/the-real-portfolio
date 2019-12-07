af30fb1d9c1c8865eace5ee5545128fd585d147c
kiamco <kiamcoo@gmail.com>
2019-12-07 09:05:45 -0800

    add projects

--- portfolio/gatsby-config.js
+++ portfolio/gatsby-config.js
@@ -9,8 +9,8 @@ module.exports = {
         {
             resolve: `gatsby-source-filesystem`,
             options: {
-                name: `images`,
-                path: `${__dirname}/src/data`,
+                name: `projects`,
+                path: `./src/data`,
             },
         },
         `gatsby-transformer-sharp`,
