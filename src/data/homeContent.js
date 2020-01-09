36d4523ecf447ac5c0389d5c35feb0b0176b2f5b
kiamco <kiamcoo@gmail.com>
2020-01-09 10:02:24 -0800

    change orders

--- src/data/homeContent.js
+++ src/data/homeContent.js
@@ -4,8 +4,7 @@ import Hangman from '../img/proj/hangman.PNG';
 import DarkMode from '../img/proj/darkmode.PNG';
 import FarmFreshAPI from '../img/proj/farmFresh.PNG'
 
-export const product = [
-    {
+export const product = [{
         product: "Questr",
         description: "Questr, an Open Source platform giving student a place they can ask questions related to their classes and professors. Which allow them to build communities that's good enough to support themselves and other individuals who don't have tutors or friends to study with.",
         image: Questr,
@@ -17,6 +16,12 @@ export const product = [
 
 export const projects = [
     {
+        product: "Fresh Farm produce API",
+        description: "An api the can be used to create an ecommerce website for farmers that want to sell their produce independently",
+        url: "https://farm-life.herokuapp.com/",
+        image: FarmFreshAPI
+    },
+    {
         product: "Hangman",
         description: "I created a hangman game, that uses React/Redux. This project was an oppurtunity to learn redux in depth because when I first started learning redux I experienced some difficulty. After I completed this project I was able gain more insight into Redux ",
         image: Hangman,
@@ -25,18 +30,12 @@ export const projects = [
     {
         product: "My Portfolio",
         description: "A website that can showcase my indie projects and personal side project. This is built with React,SASS, and Javascript.",
-        image:Portfolio
+        image: Portfolio
     },
     {
         product: "Dark mode",
         description: "the Purpose of this project is to lear how to use custom hooks and implement switch, that would change light mode to dark mode.",
         url: "https://dark-mode.kiamcoo.now.sh/",
         image: DarkMode
-    },
-    {
-        product: "Fresh Farm produce API",
-        description: "An api the can be used to create an ecommerce website for farmers that want to sell their produce independently",
-        url: "https://farm-life.herokuapp.com/",
-        image: FarmFreshAPI
     }
 ];
