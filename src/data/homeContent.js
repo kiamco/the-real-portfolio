533f5337415e69f64abea00a162e601a2e54fe38
kiamco <kiamcoo@gmail.com>
2020-01-16 23:08:24 -0800

    asdasd

--- src/data/homeContent.js
+++ src/data/homeContent.js
@@ -3,19 +3,17 @@ import Portfolio from '../img/proj/myportfolio.PNG';
 import Hangman from '../img/proj/hangman.PNG';
 import DarkMode from '../img/proj/darkmode.PNG';
 
-export const product = [
-    {
-        product: "Questr",
+export const product = [{
+        product: "Questr (BETA)",
         description: "Questr, an Open Source platform giving student a place they can ask questions related to their classes and professors. Which allow them to build communities that's good enough to support themselves and other individuals who don't have tutors or friends to study with.",
         image: Questr,
-        url: "https://questr-front-end.now.sh/dashboard"
+        url: "https://questr-front-end.now.sh"
     },
 
 ];
 
 
-export const projects = [
-    {
+export const projects = [{
         product: "Hangman",
         description: "I created a hangman game, that uses React/Redux. This project was an oppurtunity to learn redux in depth because when I first started learning redux I experienced some difficulty. After I completed this project I was able gain more insight into Redux ",
         image: Hangman,
@@ -24,7 +22,7 @@ export const projects = [
     {
         product: "My Portfolio",
         description: "A website that can showcase my indie projects and personal side project. This is built with React,SASS, and Javascript.",
-        image:Portfolio
+        image: Portfolio
     },
     {
         product: "Dark mode",
