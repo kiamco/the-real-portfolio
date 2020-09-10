38adb3904d2d1be016ac633fdcd4beb36a659efb
Kim Kiamco <kim.kiamco@menlosecurity.com>
2020-09-09 21:38:31 -0700

    added stack and change some dexriptions

--- src/components/productItem.js
+++ src/components/productItem.js
@@ -2,16 +2,17 @@ import React from 'react';
 import Directory from '../img/folder.png';
 import placeholder from '../img/settings.png'
 
-const ProductItem = ({productName, description, image, url}) => {
+const ProductItem = ({productName, description, image, url, stack}) => {
     console.log(url)
     return(
         <div className='item' onClick={()=> window.location = url}>
             <div className="left">
                 <div className="title">
                     <img src={Directory} alt=""/>
-                    <h1>{productName}</h1>
+                    <h1>{productName} </h1>
                 </div>
                 <p>{description}</p>
+                <p class="stack">Tech Stack: {stack}</p>
             </div>
             <div className="right">
                 <div className="img-container">
